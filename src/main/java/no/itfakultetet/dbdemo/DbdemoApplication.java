14ba5ae9b8b783ca7eb7a597514ce8156c36e2c4
terjebh <terje@itfakultetet.no>
2023-06-04 14:35:57 +0200

    logging til fil

--- src/main/java/no/itfakultetet/dbdemo/DbdemoApplication.java
+++ src/main/java/no/itfakultetet/dbdemo/DbdemoApplication.java
@@ -10,7 +10,6 @@ public class DbdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DbdemoApplication.class, args);
-
         @Controller
         class WebController {
             @RequestMapping(value = "/index")
