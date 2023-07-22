68d162f27980db68dc44dc270bcdf68222373cf4
Terje Berg-Hansen <terje@itfakultetet.no>
2023-07-22 12:14:58 +0200

    Endret fra textarea til div

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,20 +1,24 @@
 document.getElementById("hent").addEventListener("click",function ()  {
 if (document.getElementById("selectDB").value=="Velg Database") {
    document.getElementById("feilmelding").innerHTML="Velg en database å hente data fra ...";
-} else if (document.getElementById("query").value=="") {
+} else if (document.getElementById("queryText").innerHTML=="") {
    document.getElementById("feilmelding").innerHTML="Skriv en SQL-setning å hente data med...";
 } else {
+  let renQueryText = strip(document.getElementById("queryText").innerHTML)
+  document.getElementById("query").value = renQueryText;
   document.getElementById("sql").submit();
 }
 });
 
-document.getElementById("query").addEventListener("keyup", function (event) {
+document.getElementById("queryText").addEventListener("keyup", function (event) {
 if (event.ctrlKey && event.keyCode == 13) {
 if (document.getElementById("selectDB").value=="Velg Database") {
    document.getElementById("feilmelding").innerHTML="Velg en database å hente data fra ...";
-} else if (document.getElementById("query").value=="") {
+} else if (document.getElementById("queryText").innerHTML=="") {
      document.getElementById("feilmelding").innerHTML="Skriv en SQL-setning å hente data med...";
 } else {
+  let renQueryText = strip(document.getElementById("queryText").innerHTML)
+    document.getElementById("query").value = renQueryText;
   document.getElementById("sql").submit();
 }
 }
@@ -24,10 +28,15 @@ if (document.getElementById("selectDB").value=="Velg Database") {
 document.getElementById("selectDB").addEventListener("change", function () {
   document.getElementById("feilmelding").innerHTML="";
   document.getElementById("db").value = document.getElementById("selectDB").value;
-  document.getElementById("query").focus();
+  document.getElementById("queryText").focus();
 
   fetch("/rest/get/tablelist/"+document.getElementById("selectDB").value)
   .then(tabeller => tabeller.text())
   .then(liste => document.getElementById("tabellListe").innerHTML=liste );
 
 });
+
+function strip(html){
+   let doc = new DOMParser().parseFromString(html, 'text/html');
+   return doc.body.textContent || "";
+}
