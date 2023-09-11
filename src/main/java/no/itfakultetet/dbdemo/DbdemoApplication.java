3bdbc95e856290f850da15bb83d926ae1679060f
Terje Berg-Hansen <terje@itfakultetet.no>
2023-09-11 15:14:26 +0200

    Fixed imports

--- src/main/java/no/itfakultetet/dbdemo/DbdemoApplication.java
+++ src/main/java/no/itfakultetet/dbdemo/DbdemoApplication.java
@@ -1,12 +1,7 @@
 package no.itfakultetet.dbdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DbdemoApplication {
